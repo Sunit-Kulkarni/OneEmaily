fc8551217f14d5c6b030bb2271955907dacaac2b
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2018-01-24 00:22:56 -0500

    added nodemon

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express(); //generates new running express app. We will only be using a single app
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there buddy" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000; //dynamic port binding for heroku or dev environment
 app.listen(PORT);
