2c9242ebe4b74f83ecb348766410db5c853affe6
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2018-01-23 20:22:32 -0500

    added dynamic port binding

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express(); //generates new running express app. We will only be using a single app
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT || 5000; //dynamic port binding for heroku or dev environment
+app.listen(PORT);
