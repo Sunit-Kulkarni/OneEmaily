d55bfa7a666928dd4d1d0b1f2ce2bfb8d0ce9f60
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2018-01-23 20:51:44 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express(); //generates new running express app. We will only be using a single app
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hi: "there buddy" });
 });
 
 const PORT = process.env.PORT || 5000; //dynamic port binding for heroku or dev environment
