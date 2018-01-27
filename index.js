d334b3a35ba0dc89d1965c629c0ae8b5a8eb7c85
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2018-01-27 17:50:19 -0500

    added billing route handler and action to add credits

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/user');
 require('./services/passport');
@@ -9,6 +10,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express(); //generates new running express app. We will only be using a single app
+
+app.use(bodyParser.json()); //parses body from a request and assign to req.body property
 
 app.use(
   //wiring up middleware to our application.
@@ -23,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000; //dynamic port binding for heroku or dev environment
 app.listen(PORT);
